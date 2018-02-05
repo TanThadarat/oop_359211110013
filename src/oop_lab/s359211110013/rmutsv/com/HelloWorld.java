2df0a0bb69ef3eabf93f6f9ddcf824df90445105
TanThadarat <awayge.23@gmail.com>
2018-02-05 15:18:25 +0700

    Hello

---
+++ src/oop_lab/s359211110013/rmutsv/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package oop_lab.s359211110013.rmutsv.com;
+
+public class HelloWorld {
+    //    Create main method
+    public static void main(String[] args) {
+        System.out.println("Hello world, I'm TAN");
+        System.out.println("My name is Thadarat Fahkiat");
+//    Add information
+        System.out.println("Faculty: Management Technology,RMUTSV");
+
+    }//main
+}//class
